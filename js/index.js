dd4d20daeb3e81c7e8cf27aa7bef08893388b4d7
At1set <iliya9874329@gmail.com>
2025-07-30 15:01:45 +0300

    Добавлен кастомный курсор; Добавлены стили для игрока

--- js/index.js
+++ js/index.js
@@ -8,4 +8,22 @@ window.onload = () => {
 
   const gameManager = new GameManager(canvas, toolbar)
   gameManager.init().startGameLoop()
+
+  const customCursor = document.getElementById("custom-cursor")
+  window.addEventListener("mousemove", (e) => {
+    const cursorStyle = getComputedStyle(e.target).cursor
+    if (cursorStyle !== "none") {
+      customCursor.style.display = "none"
+    } else {
+      customCursor.style.display = "block"
+      customCursor.style.left = e.clientX + "px"
+      customCursor.style.top = e.clientY + "px"
+    }
+  })
+  window.addEventListener("mouseout", (e) => {
+    if (!e.relatedTarget && !e.toElement) {
+      // Мышь ушла из окна браузера
+      customCursor.style.display = "none"
+    }
+  })
 }
